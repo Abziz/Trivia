b385131ff84fc385b95b7518dc27e5e54c6b6047
TalAbziz <Abziz89@gmail.com>
2017-04-23 21:21:53 +0300

    30% done or something

---
+++ scripts/index.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+     getCategories($("#categories"));
+});
+
+function getCategories(select) {
+    $.ajax({
+        url: "https://opentdb.com/api_category.php",
+        context: document.body
+    }).done(function (data) {
+        categories = data.trivia_categories;
+        for (i in categories) {
+            var cat = categories[i];
+            var option = "<option value=" + cat.id + ">" + cat.name + "</option>"
+            select.append(option);
+        }
+    });
+
+
+}
